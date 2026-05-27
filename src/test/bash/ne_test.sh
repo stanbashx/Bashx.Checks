2372ae1ecc3307680cc99fdb5475717eca780698
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-05-28 00:26:48 +0300

    tests

---
+++ src/test/bash/ne_test.sh
@@ -0,0 +1,62 @@
+#!/usr/local/bin/bash
+
+SCRIPT="src/main/bash/ne.sh"
+
+echo "Running test of \"${SCRIPT}\"..."
+
+. $asserts/file/exists.sh "${SCRIPT}"
+
+# todo asserts is not empty
+# todo asserts is executable
+
+if [[ ! -s "${SCRIPT}" ]]; then
+ echo "File \"${SCRIPT}\" is empty!" >&2; exit 1
+elif [[ ! -x "${SCRIPT}" ]]; then
+ echo "File \"${SCRIPT}\" is not executable!" >&2; exit 1
+fi
+
+# todo asserts script
+
+STDERR="$(mktemp)"
+
+"${SCRIPT}" 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
+. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+:> "${STDERR}"
+
+"${SCRIPT}" '' 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
+. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+:> "${STDERR}"
+
+"${SCRIPT}" '' '' 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
+. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+:> "${STDERR}"
+
+"${SCRIPT}" '' '' '' '' 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
+. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+:> "${STDERR}"
+
+"${SCRIPT}" '' '' '' 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
+. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'No message!'
+
+:> "${STDERR}"
+
+"${SCRIPT}" 'a' 'a' 'c' 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
+. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'c'
+
+:> "${STDERR}"
+
+"${SCRIPT}" 'a' 'b' 'c' 2>"${STDERR}"; CODE=$?
+. $asserts/eq.sh "${SCRIPT}" "${CODE}" '0'
+. $asserts/empty.sh "${SCRIPT}" "$(<"${STDERR}")"
+
+rm "${STDERR}"
--- src/test/bash/unit_test.sh
+++ src/test/bash/unit_test.sh
@@ -1,8 +1,12 @@
 #!/usr/local/bin/bash
 
+if [[ ! -d "${asserts}" ]]; then
+ echo 'No asserts!'; exit 1; fi
+
 tests='src/test/bash'
 
 . $tests/eq_test.sh
+. $tests/ne_test.sh
 
 . $tests/check_coverage.sh
 
