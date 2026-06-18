d4510a2523474abc4e1f7dfd75c2489c60059001
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-18 17:40:58 +0300

    checks

--- src/test/bash/check_license.sh
+++ src/test/bash/check_license.sh
@@ -1,15 +1,9 @@
 #!/usr/local/bin/bash
 
 ISSUER='LICENSE'
-. $asserts/file/not/empty.sh "${ISSUER}"
+. $asserts/files/not_empty.sh "${ISSUER}"
 
 AUTHOR='Stanley Wintergreen'
 REGEX="Copyright 2[0-9]{3} ${AUTHOR}"
 
-# todo asserts file regex
-
-if ! grep -qE "${REGEX}" "${ISSUER}"; then
- echo "File \"${ISSUER}\" does not satisfy the regex:
----
-${REGEX}
----"; exit 1; fi
+. $asserts/files/regex.sh "${ISSUER}" "${REGEX}"
