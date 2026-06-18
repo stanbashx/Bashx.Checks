5b3ab2483f50b67de79354c1b84a16b2487c330a
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-18 18:00:55 +0300

    ne

--- src/main/bash/strings/ne.sh
+++ src/main/bash/strings/ne.sh
@@ -6,12 +6,14 @@ if [[ $# -lt 2 || $# -gt 3 ]]; then
 CHECKS_ACTUAL="$1"
 CHECKS_EXPECTED="$2"
 
+if [[ $# -eq 3 ]]; then
+ CHECKS_MESSAGE="$3"
+ if [[ -z "${CHECKS_MESSAGE}" ]]; then
+  echo 'No message!' >&2; exit 1; fi
+fi
+
 if [[ "${CHECKS_ACTUAL}" == "${CHECKS_EXPECTED}" ]]; then
- if [[ $# -eq 3 ]]; then
-  CHECKS_MESSAGE="$3"
-  if [[ -z "${CHECKS_MESSAGE}" ]]; then
-   echo 'No message!' >&2; exit 1; fi
-  echo "${CHECKS_MESSAGE}" >&2
- fi
+ if [[ -n "${CHECKS_MESSAGE}" ]]; then
+  echo "${CHECKS_MESSAGE}" >&2; fi
  exit 1
 fi
--- src/test/bash/strings/eq_test.sh
+++ src/test/bash/strings/eq_test.sh
@@ -67,28 +67,15 @@ STDERR="$(mktemp)"
 . $asserts/files/empty.sh "${STDOUT}"
 . $asserts/files/equals.sh "${STDERR}" $'c\n'
 
-"${SCRIPT}" 'a' 'b' '' >"${STDOUT}" 2>"${STDERR}"
+"${SCRIPT}" 'a' 'a' '' >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
 . $asserts/files/equals.sh "${STDERR}" $'No message!\n'
 
-echo 'Not implemented!'; exit 1 # todo
-
-"${SCRIPT}" '' '' '' 2>"${STDERR}"; CODE=$?
-. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
-. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'No message!'
-
-:> "${STDERR}"
-
-"${SCRIPT}" 'a' 'b' 'c' 2>"${STDERR}"; CODE=$?
-. $asserts/eq.sh "${SCRIPT}" "${CODE}" '1'
-. $asserts/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'c'
-
-:> "${STDERR}"
-
-"${SCRIPT}" 'a' 'a' 'c' 2>"${STDERR}"; CODE=$?
-. $asserts/eq.sh "${SCRIPT}" "${CODE}" '0'
-. $asserts/empty.sh "${SCRIPT}" "$(<"${STDERR}")"
+"${SCRIPT}" 'a' 'b' '' >"${STDOUT}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" $'No message!\n'
 
 #
 
