c1b3a9a155e4fbff56a9dc9f3ad71c9f29aec3a7
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-27 22:39:00 +0300

    wip

--- src/main/bash/fail.sh
+++ src/main/bash/fail.sh
@@ -5,7 +5,7 @@ if [[ $# -eq 1 ]]; then
  if [[ -z "${CHECKS_MESSAGE}" ]]; then
   echo 'No message!' >&2; exit 1; fi
  echo "${CHECKS_MESSAGE}" >&2
-elif [[ $# -gt 1 ]]; then
+elif [[ $# -ne 0 ]]; then
  echo 'Wrong arguments!' >&2; exit 1
 fi
 
