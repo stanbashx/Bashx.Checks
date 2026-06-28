2a31751d928223c8ec6c273614a1aa77e6724325
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-28 22:12:52 +0300

    wip

--- src/main/bash/strings/require.sh
+++ src/main/bash/strings/require.sh
@@ -6,10 +6,10 @@ if [[ $# -eq 0 ]]; then
 for ((CHECKS_INDEX=1; CHECKS_INDEX<=$#; CHECKS_INDEX++)); do
  CHECKS_ARGUMENT="${!CHECKS_INDEX}"
  if [[ -z "${CHECKS_ARGUMENT}" ]]; then
-  echo "Argument name ${CHECKS_INDEX}/$# is empty!" >&2; exit 1
+  echo "Argument ${CHECKS_INDEX}/$# is empty!" >&2; exit 1
  elif [[ ! -v "${CHECKS_ARGUMENT}" ]]; then
-  echo "Argument \"${CHECKS_ARGUMENT}\" is unset!" >&2; exit 1
+  echo "Variable \"${CHECKS_ARGUMENT}\" is unset!" >&2; exit 1
  elif [[ -z "${!CHECKS_ARGUMENT}" ]]; then
-  echo "Argument \"${CHECKS_ARGUMENT}\" is empty!" >&2; exit 1
+  echo "Variable \"${CHECKS_ARGUMENT}\" is empty!" >&2; exit 1
  fi
 done
