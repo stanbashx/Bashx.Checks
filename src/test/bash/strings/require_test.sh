f9c6e0c814eab75b9cefefcf6c00f8655be2c30b
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-28 22:17:55 +0300

    tests

--- src/test/bash/strings/require_test.sh
+++ src/test/bash/strings/require_test.sh
@@ -26,14 +26,14 @@ STDERR="$(mktemp)"
 "${SCRIPT}" ASSERTS_FOO > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument "ASSERTS_FOO" is unset!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Variable "ASSERTS_FOO" is unset!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 ASSERTS_FOO='' "${SCRIPT}" ASSERTS_FOO > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument "ASSERTS_FOO" is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Variable "ASSERTS_FOO" is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
@@ -47,42 +47,42 @@ ASSERTS_FOO='1' "${SCRIPT}" ASSERTS_FOO > "${STDOUT}" 2> "${STDERR}"
 ASSERTS_FOO='' ASSERTS_BAR='2' ASSERTS_BAZ='3' "${SCRIPT}" ASSERTS_FOO ASSERTS_BAR ASSERTS_BAZ > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument "ASSERTS_FOO" is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Variable "ASSERTS_FOO" is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 ASSERTS_FOO='1' ASSERTS_BAR='' ASSERTS_BAZ='3' "${SCRIPT}" ASSERTS_FOO ASSERTS_BAR ASSERTS_BAZ > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument "ASSERTS_BAR" is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Variable "ASSERTS_BAR" is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 ASSERTS_FOO='1' ASSERTS_BAR='2' ASSERTS_BAZ='' "${SCRIPT}" ASSERTS_FOO ASSERTS_BAR ASSERTS_BAZ > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument "ASSERTS_BAZ" is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Variable "ASSERTS_BAZ" is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 ASSERTS_FOO='1' ASSERTS_BAR='2' ASSERTS_BAZ='3' "${SCRIPT}" '' ASSERTS_BAR ASSERTS_BAZ > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument name 1/3 is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Argument 1/3 is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 ASSERTS_FOO='1' ASSERTS_BAR='2' ASSERTS_BAZ='3' "${SCRIPT}" ASSERTS_FOO '' ASSERTS_BAZ > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument name 2/3 is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Argument 2/3 is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 ASSERTS_FOO='1' ASSERTS_BAR='2' ASSERTS_BAZ='3' "${SCRIPT}" ASSERTS_FOO ASSERTS_BAR '' > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Argument name 3/3 is empty!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Argument 3/3 is empty!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
